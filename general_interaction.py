933fba674dc9b7fd70a0e7e47ab80e46f40217c8
Julius Wallnöfer <julius.wallnoefer@uibk.ac.at>
2017-11-16 14:34:07 +0100

    added linear repeater environment and related optimizations
    This commit is to be understood as the first big commit to get the repository started.
    Therefore a lot has changed here as I try to detail below:
    
    environments/linear_repeater_env.py
    a new environment that tries to replicate a 1d repeater situation
    
    Run.py
    now supports quick matplotlib plots
    now supports writing the history of the last trial to results/last_trial_history.txt
    added support for the new linear_repeater_env
    
    general_interaction.py
    will now keep track of and return the last_trial_history in single_learning_life()
    
    ps_min_agent.py
    the changes here are to be considered a band-aid solution until sparse-matrices/linked-lists are ready
    in order to deal with big percept-/action-spaces:
    	agent won't update the h-matrix after every action if it does not have to
    	agent won't update the g-matrix after every action, but only if a reward is received
    	to do this, the agent will keep track of the history since the last reward.
    	this is a mathematical trick and not to be confused with giving the agent actual memory

--- general_interaction.py
+++ general_interaction.py
@@ -1,33 +1,43 @@
+from __future__ import division, print_function
 import numpy as np
 
 class Interaction(object):
-	
-	def __init__(self, agent_type, agent, environment, video_bool, **userconfig):
-		self.agent_type = agent_type
-		self.agent = agent
-		self.env = environment
-		self.video_bool = video_bool
-		
-	def single_learning_life(self, n_trials, max_steps_per_trial):
-		learning_curve = np.zeros(n_trials)
-		for i_trial in range(n_trials):
-			reward_trial = 0
-			observation = self.env.reset()
-			for t in range(max_steps_per_trial):
-				if self.agent_type in ('PS', 'PS-basic'):
-					observation, reward, done = self.single_interaction_step_PS(observation)
-				reward_trial += float(reward)
-				if done:
-					learning_curve[i_trial] = reward_trial/(t+1)
-					break
-		return learning_curve
-		
-	def single_interaction_step_PS(self, observation):
-		percept_now = self.agent.percept_preprocess(observation)
-		action = self.agent.policy(percept_now)
-		#print(observation, percept_now, action) #, end=" "
-		observation, reward, done = self.env.move(action)
-		#print(reward)
-		self.agent.learning(reward)
-		#print(observation, reward, done) # print history
-		return observation, reward, done
+    
+    def __init__(self, agent_type, agent, environment, video_bool, **userconfig):
+        self.agent_type = agent_type
+        self.agent = agent
+        self.env = environment
+        self.video_bool = video_bool
+        
+    def single_learning_life(self, n_trials, max_steps_per_trial):
+        learning_curve = np.zeros(n_trials)
+        for i_trial in range(n_trials):
+#            print(i_trial)
+            reward_trial = 0
+            observation = self.env.reset()
+            if i_trial == n_trials - 1:
+                last_trial_history = []
+            for t in range(max_steps_per_trial):
+                old_observation = observation
+                if self.agent_type in ('PS', 'PS-basic'):
+                    observation, reward, done, action = self.single_interaction_step_PS(observation)
+                reward_trial += float(reward)
+                if i_trial == n_trials - 1:
+                    last_trial_history += [(old_observation, action)]
+                if done:
+                    learning_curve[i_trial] = reward_trial/(t+1)
+                    break
+        return learning_curve, last_trial_history
+        
+    def single_interaction_step_PS(self, observation):
+        percept_now = self.agent.percept_preprocess(observation)
+        action = self.agent.policy(percept_now)
+#        print("Agent policy step finished.")
+        #print(observation, percept_now, action) #, end=" "
+        observation, reward, done = self.env.move(action)
+#        print("Environment move finished.")
+        #print(reward)
+        self.agent.learning(reward)
+#        print("Agent learning step finished.")
+        #print(observation, reward, done) # print history
+        return observation, reward, done, action
