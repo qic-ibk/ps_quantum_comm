64138cd9a3094eb6f75e4eda1f88c4467c26004b
Julius Wallnöfer <julius.wallnoefer@uibk.ac.at>
2017-11-20 16:37:55 +0100

    added an option to use time-based glow to the agent
    This change is fully backwards compatible.
    Still missing: Corresponding Functionality in Interaction and Environment.

--- agents/ps_min_agent.py
+++ agents/ps_min_agent.py
@@ -11,7 +11,7 @@ class BasicPSAgent(object):
     """ policy_type - 'standard' or 'softmax' """
     """ ps_alpha - constant """
     
-    def __init__(self, n_actions, n_percepts_multi, ps_gamma, ps_eta, policy_type, ps_alpha, **userconfig):
+    def __init__(self, n_actions, n_percepts_multi, ps_gamma, ps_eta, policy_type, ps_alpha, time_glow=False, **userconfig):
         self.n_actions = n_actions
         self.n_percepts_multi = n_percepts_multi
         self.n_percepts = np.prod(n_percepts_multi)
@@ -19,11 +19,15 @@ class BasicPSAgent(object):
         self.config = {
             "ps_gamma" : ps_gamma,
             "ps_eta" : ps_eta,
-            "ps_alpha" : ps_alpha}
+            "ps_alpha" : ps_alpha,
+            "time_aware_glow" : time_glow}
         self.config.update(userconfig)
         self.h_matrix = np.ones((self.n_actions, self.n_percepts))
         self.g_matrix = np.zeros((self.n_actions, self.n_percepts))
         self.history_since_last_reward = []
+        if time_glow == True: # prepare some properties the time dependant glow will need
+            self.time_before = -1
+            self.history_since_last_time_step = []
         
     def percept_preprocess(self, observation): # preparing for creating a percept
         return self.mapping_to_1d(observation)
@@ -38,14 +42,23 @@ class BasicPSAgent(object):
         return percept
 
     def update_g_matrix(self):
-        n = len(self.history_since_last_reward)
-        self.g_matrix = (1-self.config["ps_eta"])**n * self.g_matrix
-        for i in range(n):
-            action, percept = self.history_since_last_reward[i]
-            self.g_matrix[action,percept] = (1-self.config["ps_eta"])**(n - 1 - i)
-        self.history_since_last_reward = []
+        if self.config["time_aware_glow"] == True:
+            self.history_since_last_reward += [self.history_since_last_time_step]
+            n = len(self.history_since_last_reward)
+            self.g_matrix = (1-self.config["ps_eta"])**n * self.g_matrix
+            for i, sub_history in enumerate(self.history_since_last_reward):
+                for action,percept in sub_history:
+                    self.g_matrix[action,percept] = (1-self.config["ps_eta"])**(n - 1 - i)
+            self.history_since_last_time_step = []
+            self.history_since_last_reward = []
+        else:
+            n = len(self.history_since_last_reward)
+            self.g_matrix = (1-self.config["ps_eta"])**n * self.g_matrix
+            for i,[action,percept] in enumerate(self.history_since_last_reward):
+                self.g_matrix[action,percept] = (1-self.config["ps_eta"])**(n - 1 - i)
+            self.history_since_last_reward = []
     
-    def policy(self, percept_now): # policy + g-matrix update
+    def policy(self, percept_now, time_now = None): # policy + g-matrix update
         config = self.config
         # action selection
         if self.policy_type == 'standard':
@@ -53,18 +66,20 @@ class BasicPSAgent(object):
             p_vector_now = h_vector_now / np.sum(h_vector_now)
         if self.policy_type == 'softmax':
             h_vector_now = self.config["ps_alpha"] * self.h_matrix[:, percept_now]
-#            print("h_vector step finished")
             h_vector_now_mod = h_vector_now - np.max(h_vector_now)
-#            print("h_vector_mod step finished")
-            p_vector_now = np.exp(h_vector_now_mod) / np.sum(np.exp(h_vector_now_mod))
-#            print("p_vector step finished")       
+            p_vector_now = np.exp(h_vector_now_mod) / np.sum(np.exp(h_vector_now_mod))   
         action = np.random.choice(np.arange(self.n_actions), 1, p=p_vector_now)[0]
-#        print("action selected")
 #        # internally update the g-matrix
 #        self.g_matrix = (1 - self.config["ps_eta"]) * self.g_matrix 
 #        self.g_matrix[action, percept_now] = 1
-        self.history_since_last_reward += [(action,percept_now)]
-#        print("g_matrix update finished")
+        if self.config["time_aware_glow"] == True:
+            if time_now != self.time_before:
+                self.history_since_last_reward += [self.history_since_last_time_step]
+                self.history_since_last_time_step = []
+                self.time_before = time_now
+            self.history_since_last_time_step += [(action,percept_now)]
+        else:
+            self.history_since_last_reward += [(action,percept_now)]
         return action
         
     def learning(self, reward_now):
