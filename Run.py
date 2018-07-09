13f03dc6cdcbbb98e00fed24768a690c8fd93d18
Julius Wallnöfer <33934646+jwallnoefer@users.noreply.github.com>
2018-07-09 13:54:36 +0200

    Clean up run-file. Agents and environments are now modules.
    Also added __init__.py files for python2 compatibility.

--- Run.py
+++ Run.py
@@ -1,16 +1,18 @@
-# import platform
-# print('Python version:', platform.python_version())
+# -*- coding: utf-8 -*-
+"""This file controls the whole setup.
 
+It initializes all the relevant agents, interactions and environments and
+remembers all the different variable configurations that each of them needs.
+"""
 from __future__ import division, print_function
-import os, inspect  # for current directory
+import os  # for current directory
 import numpy as np
-import sys  # for importing agents and environments
-sys.path.insert(0, 'agents')
-sys.path.insert(0, 'environments')
 import matplotlib.pyplot as plt
 import itertools
 import json
 from time import time
+# Import interaction class
+from general_interaction import Interaction
 
 start_time = time()
 
@@ -21,34 +23,32 @@ max_steps_per_trial = 10000
 colors = itertools.cycle(["r", "b", "g"])
 
 # Choose an environment
-environment_here = 'linearRepeater_less_actions' # don't forget to set ps_eta = 1 for Invasion_Game, Driver_Game (they don't have temporal correlations)
+environment_here = 'linearRepeater_less_actions'  # don't forget to set ps_eta = 1 for Invasion_Game, Driver_Game (they don't have temporal correlations)
 # Choose an agent
 agent_here = 'PS-basic'
 
 # Import an environment
 if environment_here == 'Driver_Game':
-    from driver_game_env import *
+    from environments.driver_game_env import TaskEnvironment
 elif environment_here == 'Invasion_Game':
-    from invasion_game_env import *
+    from environments.invasion_game_env import TaskEnvironment
 elif environment_here == 'Neverending_Color':
-    from neverending_color_env import *
+    from environments.neverending_color_env import TaskEnvironment
 elif environment_here == 'Quantum_Networks_1':
-    from q_network_env_1 import *
+    from environments.q_network_env_1 import TaskEnvironment
 elif environment_here == 'Quantum_Networks_2':
-    from q_network_env_2 import *
+    from environments.q_network_env_2 import TaskEnvironment
 elif environment_here == 'JW_GridWorld':
-    from JW_GridWorld_env import *
+    from environments.JW_GridWorld_env import TaskEnvironment
 elif environment_here == 'linearRepeater':
-    from linear_repeater_env import *
+    from environments.linear_repeater_env import TaskEnvironment
 elif environment_here == 'linearRepeater_less_actions':
-    from linear_repeater_less_actions import *
+    from environments.linear_repeater_less_actions import TaskEnvironment
 
 # Import an agent
 if agent_here == 'PS-basic':
-    from ps_min_agent import * # import the basic PS agent
+    from agents.ps_min_agent import BasicPSAgent  # import the basic PS agent
 
-# Import interaction class
-from general_interaction import *
 
 # parameter to 1D encoding
 n_param_scan = 1
@@ -64,13 +64,13 @@ for i_param_scan in range(n_param_scan):
         if environment_here in ('Driver_Game', 'Invasion_Game', 'JW_GridWorld'):
             env = TaskEnvironment()
         elif environment_here == 'Neverending_Color':
-            env = TaskEnvironment(2, n_trials, 1) # n_actions, n_trials, reward_value
+            env = TaskEnvironment(2, n_trials, 1)  # n_actions, n_trials, reward_value
         elif environment_here == 'Quantum_Networks_1':
-            env = TaskEnvironment(2, 2) # n_qubits, line_length
+            env = TaskEnvironment(2, 2)  # n_qubits, line_length
         elif environment_here == 'Quantum_Networks_2':
-            env = TaskEnvironment(2, 2) # n_qubits, line_length
+            env = TaskEnvironment(2, 2)  # n_qubits, line_length
         elif environment_here in ('linearRepeater', 'linearRepeater_less_actions'):
-            env = TaskEnvironment(tracks_time = False)
+            env = TaskEnvironment(tracks_time=False)
             tg = False
         # Inialize an agent
         if agent_here == 'PS-basic':
@@ -79,14 +79,14 @@ for i_param_scan in range(n_param_scan):
 
         interaction = Interaction(agent_here, agent, env)
         learning_curve, last_trial_history = interaction.single_learning_life(n_trials, max_steps_per_trial)
-        average_learning_curve += learning_curve/n_agents
+        average_learning_curve += learning_curve / n_agents
 #        plt.scatter(np.arange(1,n_trials+1),1/(learning_curve + pow(10,-10)),c=next(colors))
 
-##    print('Total average reward per action\n', average_learning_curve)
-##    plt.plot(np.arange(1,n_trials+1),average_learning_curve)
-##    plt.show()
+# #    print('Total average reward per action\n', average_learning_curve)
+# #    plt.plot(np.arange(1,n_trials+1),average_learning_curve)
+# #    plt.show()
 #    print('Number of steps to the goal (only makes sense when the goal is always reached in each trial, e.g. grid world or q. networks)\n', 1/(average_learning_curve + pow(10,-10)) ) # pow(10,-10) to awoid division by 0
-##    plt.cla()
+# #    plt.cla()
 #    plt.scatter(np.arange(1,n_trials+1),1/(average_learning_curve + pow(10,-10)))
 #    plt.ylim(0,10000)
 #    plt.grid()
@@ -102,7 +102,7 @@ if n_agents == 1:
     with open(current_file_directory + "/results/last_trial_history.txt", "w") as f:
         json.dump(last_trial_history, f)
 
-## Saving files
+# # Saving files
 # current_file_directory = os.path.dirname(os.path.abspath(__file__))
 # if n_agents == 1:
 #    np.savetxt(current_file_directory+'/results'+'/h_matrix', agent.h_matrix, fmt='%.2f', delimiter=',')
