650b7f435e70600c9d1cbe6f5a1baa25186c8787
Julius Wallnöfer <33934646+jwallnoefer@users.noreply.github.com>
2018-07-09 19:24:23 +0200

    Restores time_glow functionality and improves tests.
    Modified general_interaction.py so it supports the TimeGlowAgent
    Tests are now concise and no longer include leftovers from Run.py
    Tests could be improved by:
     * actually testing for functionalities
     * or at least making sure that agent will get a reward at least
       once to actually test the h-matrix update

--- general_interaction.py
+++ general_interaction.py
@@ -11,18 +11,20 @@ class Interaction(object):
 
     def single_learning_life(self, n_trials, max_steps_per_trial):
         learning_curve = np.zeros(n_trials)
+        reward = 0
+        info = {}
         for i_trial in range(n_trials):
             reward_trial = 0
             if hasattr(self.env, "tracks_time") and self.env.tracks_time is True:
                 observation, time_now = self.env.reset()
+                info = {"time_now": time_now}
             else:
                 observation = self.env.reset()
-                time_now = None
             if i_trial == n_trials - 1:
                 last_trial_history = []
             for t in range(max_steps_per_trial):
                 old_observation = observation
-                observation, reward, done, action, time_now = self.single_interaction_step_PS(observation, time_now)
+                observation, reward, done, action, info = self.single_interaction_step_PS(observation, reward, info)
                 reward_trial += float(reward)
                 if i_trial == n_trials - 1:
                     last_trial_history += [(old_observation, action)]
@@ -31,12 +33,11 @@ class Interaction(object):
                     break
         return learning_curve, last_trial_history
 
-    def single_interaction_step_PS(self, observation, time_now=None):
-        action = self.agent.policy(observation)
+    def single_interaction_step_PS(self, observation, reward, info):
+        action = self.agent.deliberate_and_learn(observation, reward, info)
         if hasattr(self.env, "tracks_time") and self.env.tracks_time is True:
             observation, reward, done, time_now = self.env.move(action)
+            info = {"time_now": time_now}
         else:
             observation, reward, done = self.env.move(action)
-            time_now = None
-        self.agent.learning(reward, time_now)
-        return observation, reward, done, action, time_now
+        return observation, reward, done, action, info
