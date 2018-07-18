b57927f6893f65c3fd7723acb095ad691b16569c
Julius Wallnöfer <33934646+jwallnoefer@users.noreply.github.com>
2018-07-18 17:59:18 +0200

    added object oriented clip-network brain
    so far it can only handle the two-layers network, which is NOT the
    use case for it

--- agents/ps_agent_flexible_percepts.py
+++ agents/ps_agent_flexible_percepts.py
@@ -8,26 +8,29 @@ from time import time
 class FlexiblePerceptsPSAgent(BasicPSAgent):
     """PS Agent without predefined percepts
     """
-    def __init__(self, n_actions, ps_gamma, ps_eta, policy_type, ps_alpha, matrix_type="dense"):
-        self.agent_wait_time = time()
-        self.n_actions = n_actions
-        self.n_percepts = 0  # no initial preprogrammed percepts
-        self.policy_type = policy_type
-
-        self.ps_gamma = ps_gamma
-        self.ps_eta = ps_eta
-        self.ps_alpha = ps_alpha
-
-        if matrix_type == "dense":
-            from .brains.dense_brain import DenseBrain
-            self.brain = DenseBrain(self.n_actions, n_percepts=0)
-        elif matrix_type == "sparse":
-            from .brains.sparse_brain import SparseBrain
-            self.brain = SparseBrain(self.n_actions, n_percepts=0)
-        else:
-            raise ValueError("%s is not a supported matrix_type" % matrix_type)
-
-        self.history_since_last_reward = []
+    # def __init__(self, n_actions, ps_gamma, ps_eta, policy_type, ps_alpha, brain_type="dense"):
+    #     self.agent_wait_time = time()
+    #     self.n_actions = n_actions
+    #     self.n_percepts = 0  # no initial preprogrammed percepts
+    #     self.policy_type = policy_type
+    #
+    #     self.ps_gamma = ps_gamma
+    #     self.ps_eta = ps_eta
+    #     self.ps_alpha = ps_alpha
+    #
+    #     if brain_type == "dense":
+    #         from .brains.dense_brain import DenseBrain
+    #         self.brain = DenseBrain(self.n_actions, n_percepts=0)
+    #     elif brain_type == "sparse":
+    #         from .brains.sparse_brain import SparseBrain
+    #         self.brain = SparseBrain(self.n_actions, n_percepts=0)
+    #     else:
+    #         raise ValueError("%s is not a supported brain_type" % brain_type)
+    #
+    #     self.history_since_last_reward = []
+    #     self.percept_dict = {}
+    def __init__(self, n_actions, ps_gamma, ps_eta, policy_type, ps_alpha, brain_type="dense"):
+        BasicPSAgent.__init__(self, n_actions, [0], ps_gamma, ps_eta, policy_type, ps_alpha, brain_type)
         self.percept_dict = {}
 
     def _percept_preprocess(self, observation):
