86ea5855125eb3485fbab304c0cdbf751a7fa8f2
Julius Wallnöfer <33934646+jwallnoefer@users.noreply.github.com>
2018-10-22 17:52:45 +0200

    manual copy of teleportation run files

--- run/run_teleportation_universal.py
+++ run/run_teleportation_universal.py
@@ -1,8 +1,10 @@
 from __future__ import division, print_function
+import os, sys; sys.path.insert(0, os.path.abspath("."))
 from time import time
 from environments.teleportation_universal_env import TaskEnvironment as TeleportationUniversalEnv
 # from environments.teleportation_env import TaskEnvironment as TeleportationEnv
-from agents.ps_agent_flexible_percepts import FlexiblePerceptsPSAgent
+# from agents.ps_agent_flexible_percepts import FlexiblePerceptsPSAgent
+from agents.ps_agent_changing_actions import ChangingActionsPSAgent
 from general_interaction import Interaction
 from multiprocessing import Pool
 import numpy as np
@@ -18,8 +20,8 @@ result_path = "results/teleportation/universal_gates/raw/"
 def run_teleportation(i, eta, label_multiplicator=10, sparsity=10):
     np.random.seed()
     env = TeleportationUniversalEnv()
-    agent = FlexiblePerceptsPSAgent(env.n_actions, ps_gamma=0, ps_eta=eta, policy_type="softmax", ps_alpha=1, brain_type="dense")
-    interaction = Interaction(agent_type="FlexiblePerceptsPSAgent", agent=agent, environment=env)
+    agent = ChangingActionsPSAgent(env.n_actions, ps_gamma=0, ps_eta=eta, policy_type="softmax", ps_alpha=1, brain_type="dense")
+    interaction = Interaction(agent=agent, environment=env)
     res = interaction.single_learning_life(n_trials=n_trials, max_steps_per_trial=50)
     learning_curve = res["learning_curve"]
     success_list = np.ones(len(learning_curve), dtype=np.int)
@@ -38,20 +40,26 @@ def get_label_multiplicator(eta):
     return 10**(len(str(eta)) - 2)
 
 
-def run_function(eta):
-    def run(i):
-        run_teleportation(i, eta, label_multiplicator=get_label_multiplicator(eta))
+class RunCallable(object):  # this solution is necessary because only top-level objects can be pickled
+    def __init__(self, eta):
+        self.eta = eta
 
-    return run
+    def __call__(self, i):
+        return run_teleportation(i, self.eta, label_multiplicator=get_label_multiplicator(self.eta))
+
+
+# def callback_error(result):
+#     print('error', result)
 
 
 if __name__ == "__main__":
     start_time = time()
     p = Pool(processes=num_processes)
     for eta in etas:
-        if not os.path.exists(result_path + "eta_%d/" % get_label_multiplicator(eta)):
-            os.makedirs(result_path + "eta_%d/" % get_label_multiplicator(eta))
-        p.map_async(run_function(eta), np.arange(num_agents))
+        if not os.path.exists(result_path + "eta_%d/" % (eta * get_label_multiplicator(eta))):
+            os.makedirs(result_path + "eta_%d/" % (eta * get_label_multiplicator(eta)))
+        my_callable = RunCallable(eta)
+        p.map_async(my_callable, np.arange(num_agents))
     p.close()
     p.join()
     print("The whole script took %.2f minutes." % ((time() - start_time) / 60))
