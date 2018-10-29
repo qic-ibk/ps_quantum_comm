0084da2aebe6612dd1e5db524a0d840083cc4854
Julius Wallnöfer <33934646+jwallnoefer@users.noreply.github.com>
2018-10-29 18:10:25 +0100

    copy over interaction improvements

--- general_interaction.py
+++ general_interaction.py
@@ -8,7 +8,7 @@ class Interaction(object):
         self.agent = agent
         self.env = environment
 
-    def single_learning_life(self, n_trials, max_steps_per_trial, return_last_trial_history=False, env_statistics={}):
+    def single_learning_life(self, n_trials, max_steps_per_trial, return_last_trial_history=True, env_statistics={}, verbose_trial_count=False):
         """Run consecutive trials between agent and environment.
 
         Parameters
@@ -18,10 +18,12 @@ class Interaction(object):
         max_steps_per_trial : int
             limits the maximum number of steps per trial
         return_last_trial_history : bool
-            whether to include the last trial in the output (default: False)
+            whether to include the last trial in the output (default: True)
         env_statistics : dict
             keys are used as keys for the `res` dict
             values are callables, usually environment methods
+        verbose_trial_count : bool
+            If True, prints a reminder every 1000 trials. (default: False)
 
         Returns
         -------
@@ -36,16 +38,17 @@ class Interaction(object):
             - other keys as specified by `env_statistics` : list
         """
         learning_curve = np.zeros(n_trials)
+        step_curve = np.zeros(n_trials, dtype=np.int)
         reward = 0
-        done = 0
+        episode_finished = 0
         info = {}
         res = {}
         for stat in env_statistics:
             res[stat] = []
         for i_trial in range(n_trials):
             reward_trial = 0
-            # if i_trial % 1000 == 0:
-            #     print("Interaction is now starting trial %d of %d." % (i_trial, n_trials))
+            if verbose_trial_count and i_trial % 1000 == 0:
+                print("Interaction is now starting trial %d of %d." % (i_trial, n_trials))
             setup = self.env.reset()
             if not isinstance(setup, tuple):
                 observation = setup
@@ -54,26 +57,28 @@ class Interaction(object):
                 info.update(setup[1])
             if i_trial == n_trials - 1:
                 last_trial_history = []
-            for t in range(max_steps_per_trial):
+            for i_step in range(max_steps_per_trial):
                 old_observation = observation
-                observation, reward, done, action, info = self.single_interaction_step_PS(observation, reward, done, info)
+                observation, reward, episode_finished, action, info = self.single_interaction_step_PS(observation, reward, episode_finished, info)
                 reward_trial += float(reward)
                 if return_last_trial_history and i_trial == n_trials - 1:
                     last_trial_history += [(old_observation, action)]
-                if done:
-                    learning_curve[i_trial] = reward_trial / (t + 1)
-                    for stat in env_statistics:
-                        res[stat] += [env_statistics[stat]()]
+                if episode_finished:
                     break
+            learning_curve[i_trial] = reward_trial / i_step
+            step_curve[i_trial] = i_step
+            for stat in env_statistics:
+                res[stat] += [env_statistics[stat]()]
         res["learning_curve"] = learning_curve
+        res["step_curve"] = step_curve
         if return_last_trial_history:
             res["last_trial_history"] = last_trial_history
         return res
 
-    def single_interaction_step_PS(self, observation, reward, done, info):
-        action = self.agent.deliberate_and_learn(observation, reward, done, info)
+    def single_interaction_step_PS(self, observation, reward, episode_finished, info):
+        action = self.agent.deliberate_and_learn(observation, reward, episode_finished, info)
         res = self.env.move(action)
         if len(res) == 3:  # backwards compatibility
             res += ({},)
-        observation, reward, done, info = res
-        return observation, reward, done, action, info
+        observation, reward, episode_finished, info = res
+        return observation, reward, episode_finished, action, info
