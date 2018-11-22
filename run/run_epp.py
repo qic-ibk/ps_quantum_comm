7bbd31f4521c0f59e94d024f4f97c8def8663ece
Julius Wallnöfer <33934646+jwallnoefer@users.noreply.github.com>
2018-11-22 16:13:44 +0100

    updated epp run files

--- run/run_epp.py
+++ run/run_epp.py
@@ -4,24 +4,56 @@ from environments.epp_env import EPPEnv
 from meta_analysis_interaction import MetaAnalysisInteraction
 from agents.ps_agent_changing_actions import ChangingActionsPSAgent
 import matplotlib.pyplot as plt
+from multiprocessing import Pool
+from time import time
 import numpy as np
+import os
+import traceback
 
-eta = 0
-n_agents = 1
+
+def assert_dir(path):
+    if not os.path.exists(path):
+        os.makedirs(path)
+
+
+num_processes = 64  # change according to cluster computer you choose
+num_agents = 100
 n_trials = 120000
+eta = 0
+result_path = "results/epp/raw/"
 
-results = []
-for i_agent in range(n_agents):
+
+def run_epp(i, sparsity=10):
+    np.random.seed()
     env = EPPEnv()
-    agent = ChangingActionsPSAgent(env.n_actions, ps_gamma=0, ps_eta=eta, policy_type="softmax", ps_alpha=1, brain_type="dense")
+    agent = ChangingActionsPSAgent(env.n_actions, ps_gamma=0, ps_eta=eta, policy_type="softmax", ps_alpha=1, brain_type="dense")  # glow reset is handled by MetaAnalysis Interaction
     interaction = MetaAnalysisInteraction(agent, env)
-    res = interaction.single_learning_life(n_trials, True)
-    results += [res]
-# print(res["reward_curve"])
-for res in results:
-    reward_curve = res["reward_curve"] + 10**-30
-    plt.scatter(np.arange(len(reward_curve)), reward_curve)
-    # plt.ylim(10**-30, np.max(reward_curve))
-    # plt.yscale("log")
-    plt.ylim = (0, 1.05)
-    plt.show()
+    res = interaction.single_learning_life(n_trials, verbose_trial_count=False)
+    reward_curve = res["reward_curve"]
+    if sparsity != 1:
+        reward_curve = reward_curve[::sparsity]
+    np.save(result_path + "reward_curve_%d.npy" % i, reward_curve)
+
+
+class RunCallable(object):  # this solution is necessary because only top-level objects can be pickled
+    def __init__(self):
+        pass
+
+    def __call__(self, i):
+        try:
+            return run_epp(i)
+        except Exception:
+            print("Exception occured in child process")
+            traceback.print_exc()  # so exception in child process gets output on older python versions
+            raise
+
+
+if __name__ == "__main__":
+    start_time = time()
+    p = Pool(processes=num_processes)
+    assert_dir(result_path)
+    my_callable = RunCallable()
+    p.map_async(my_callable, np.arange(num_agents))
+    p.close()
+    p.join()
+    print("The whole script took %.2f minutes." % ((time() - start_time) / 60))
