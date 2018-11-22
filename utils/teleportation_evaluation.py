86ce6142d35efe9d5274a014f77abff8ebfa8169
Julius Wallnöfer <33934646+jwallnoefer@users.noreply.github.com>
2018-11-22 15:08:38 +0100

    manual update to results version for teleportation files

--- utils/teleportation_evaluation.py
+++ utils/teleportation_evaluation.py
@@ -1,339 +1,119 @@
-"""Evaluate results from teleportation_env
+"""Evaluate results from teleportation_env and teleportation_universal_env.
 """
 
+from __future__ import division, print_function
 import numpy as np
 import matplotlib.pyplot as plt
+import os
 
 num_agents = 100
-# etas = [0.1, 0.15, 0.2, 0.25, 0.3, 0.35, 0.4, 0.45, 0.5]
-result_path = "results/teleportation/clifford_gates/raw/"
-# result_path_universal = "results/teleportation/universal_gates/raw/"
+sparsity = 10
+etas = [0.1, 0.15, 0.2, 0.25, 0.3, 0.35, 0.4, 0.45, 0.5, 0.55, 0.6, 0.65, 0.7]
+result_path_clifford = "results/teleportation/clifford_gates/raw/"
+result_path_universal = "results/teleportation/universal_gates/raw/"
+plot_path_clifford = "results/teleportation/clifford_gates/plot_ready/"
+plot_path_universal = "results/teleportation/universal_gates/plot_ready/"
 
 
-def get_step_dicts(path, num=100):
+def assert_dir(path):
+    if not os.path.exists(path):
+        os.makedirs(path)
+
+
+def get_label_multiplicator(eta):
+    return 10**(len(str(eta)) - 2)
+
+
+def get_step_curves(path, num=100, to_txt_path=None):
     res = []
     for i in range(num):
-        print(i)
-        # try:
-        #     with open(path + "/step_curve_%d.txt" % i, "r") as f:
-        #         step_curve = np.loadtxt(f)
-        # except FileNotFoundError:
-        #     print(path + "/step_curve_%d.txt" + " is missing")
-        #     continue
-        step_curve = np.load(path + "/step_curve_%d.npy" % i)
-        a = step_curve[-2000:]
-        res += [dict(zip(*np.unique(a, return_counts=True)))]
+        step_curve = np.load(path + "step_curve_%d.npy" % i)
+        res += [step_curve]
+        if to_txt_path is not None:
+            assert_dir(to_txt_path)
+            np.savetxt(to_txt_path + "step_curve_%d.txt" % i, step_curve, fmt="%-d")
     return res
 
 
-def count_solutions(count_dict):
-    count_dict = {k: v if int(k) != 0 else 0 for k, v in count_dict.items()}
-    val = np.array(list(count_dict.values()))
-    num_solutions = np.sum((400 < val) * (val < 600)) + 2 * np.sum((800 < val) * (val < 1200))
-    return num_solutions
+def count_dict(step_curve):
+    a = step_curve[-400:]  # this assumes sparsity 10 of step curves
+    return dict(zip(*np.unique(a, return_counts=True)))
 
 
-def solution_steps(count_dict):
-    auxlist = [(v, k) for k, v in count_dict.items()]
-    auxsteplist = []
-    for v, k in auxlist:
-        if 400 < v < 600 and int(k) != 0:
-            auxsteplist += [int(k)]
-        elif 800 < v < 1200 and int(k) != 0:
-            auxsteplist += [int(k), int(k)]
-    auxsteplist += [0] * (4 - len(auxsteplist))
-    return auxsteplist
+# def has_all_solutions(step_curve, failure_number=10000):
+#     a = step_curve[-400:]
+#     return np.all(a != failure_number)
 
 
-def cumulative_steps(count_dict):
-    auxlist = [(v, k) for k, v in count_dict.items()]
-    auxcount = 0
-    auxsteps = 0
-    for v, k in auxlist:
-        if 400 < v < 600 and int(k) != 0:
-            auxcount += 1
-            auxsteps += int(k)
-        elif 800 < v < 1200 and int(k) != 0:
-            auxcount += 2
-            auxsteps += 2 * int(k)
-    if auxcount == 4:
-        return auxsteps
+def cumulative_steps(count_dict, failure_number=10000):
+    auxlist = [(v, int(k)) for k, v in count_dict.items()]
+    if np.any([length == failure_number for count, length in auxlist]):
+        return np.nan
+    if len(auxlist) == 4:
+        return np.sum([length for count, length in auxlist])
+    elif len(auxlist) == 3:
+        aux = max(auxlist, key=lambda x: x[0])
+        length_list = [length for count, length in auxlist] + [aux[1]]
+        return np.sum([length_list])
     else:
-        return 0
+        raise ValueError("Could not compute cumulative_steps for " + repr(count_dict))
 
 
-count_solutions_vectorized = np.vectorize(count_solutions, otypes=[np.int])
-cumulative_steps_vectorized = np.vectorize(cumulative_steps, otypes=[np.int])
-
-
-def solution_steps_vectorized(mylist):
-    return np.array([solution_steps(dict) for dict in mylist]).flatten()
-
-
-if __name__ == "__main__":
-    dict_lists = [get_step_dicts(result_path + "eta_%d" % k, num=num_agents) for k in [1, 15, 2, 25, 3, 4, 5]]  # [1, 15, 2, 25, 3, 35, 4, 45, 5]]
-    solution_count_array = [count_solutions_vectorized(dict_list) for dict_list in dict_lists]
-    # solution_steps_array = [solution_steps_vectorized(dict_list) for dict_list in dict_lists]
-    cumulative_steps_array = [cumulative_steps_vectorized(dict_list) for dict_list in dict_lists]
-
-    plt.hist(solution_count_array, bins=[-0.5, 0.5, 1.5, 2.5, 3.5, 4.5], label=["eta=0.1", "eta=0.15", "eta=0.2", "eta=0.3", "eta=0.4", "eta=0.5"])
-    plt.legend()
-    plt.title("128 agents, 60k trials")
-    plt.xlabel("No. of solutions found")
-    plt.ylabel("No. of agents")
-    plt.savefig("presentation_solution_count.png")
-    plt.show()
-    # plt.hist(solution_steps_array, bins=np.arange(-0.5, 22.5, 1))#, label=["eta=0.1", "eta=0.2", "eta=0.3", "eta=0.4", "eta=0.5", "eta=0.15"])
-    # # plt.legend()
-    # plt.xticks(np.arange(0, 21))
-    # plt.xlabel("Length of solution")
-    # plt.ylabel("No. of solutions")
-    # plt.show()
-    plt.hist(cumulative_steps_array, bins=np.arange(24.5, 50.5, 1), label=["eta=0.1", "eta=0.15", "eta=0.2", "eta=0.3", "eta=0.4", "eta=0.5"])
-    plt.legend()
-    plt.title("128 agents, 60k trials")
-    plt.xlabel("Cumulative length of agents with 4 solutions")
-    plt.ylabel("No. of agents")
-    plt.savefig("presentation_cumulative_steps.png")
-    plt.show()
-
-    cumulative_steps_array = [csteps[csteps != 0] for csteps in cumulative_steps_array]
-    amounts = [len(csteps) / 128.0 for csteps in cumulative_steps_array]
-    my_array = [np.sum(csteps) / len(csteps) for csteps in cumulative_steps_array]
+def evaluate(result_path, plot_path):
+    average_length_list = []
+    agent_fraction_list = []
+    for eta in etas:
+        eta_plot_path = plot_path + "eta_%d/" % (eta * get_label_multiplicator(eta))
+        eta_result_path = result_path + "eta_%d/" % (eta * get_label_multiplicator(eta))
+        assert_dir(eta_plot_path)
+        step_curves = get_step_curves(eta_result_path, num=num_agents, to_txt_path=eta_plot_path)
+        count_dicts = [count_dict(step_curve) for step_curve in step_curves]
+        cumulative_steps_list = np.array([cumulative_steps(count) for count in count_dicts])
+        aux = cumulative_steps_list[np.logical_not(np.isnan(cumulative_steps_list))]
+        average_length = np.sum(aux, axis=0) / len(aux)
+        agent_fraction = len(aux) / num_agents
+        average_length_list += [average_length]
+        agent_fraction_list += [agent_fraction]
+        # first plot is the example step_curve
+        my_example = step_curves[0]
+        my_example[my_example == 10000] = 50
+        np.savetxt(eta_plot_path + "example_agent_sparsity_%d.txt" % sparsity, my_example, fmt="%-d")
+        plt.scatter(np.arange(1, len(my_example) * sparsity + 1, sparsity), my_example)
+        plt.title("Example agent for η=%.2f" % eta)
+        plt.xlabel("Trial number")
+        plt.ylabel("Number of steps")
+        plt.savefig(eta_plot_path + "example_agent.png")
+        plt.show()
+        # second plot is the averages, this is missing the variances
+        # still not convinced this is a the right way to do averages and variances
+        for curve in step_curves:
+            curve[curve == 10000] = 50
+        average_curve = np.sum(step_curves, axis=0) / num_agents
+        np.savetxt(eta_plot_path + "average_curve_sparsity_%d.txt" % sparsity, average_curve, fmt="%.6f")
+        plt.plot(np.arange(1, len(average_curve) * sparsity + 1, sparsity), average_curve)
+        plt.title("Average of %d agents for η=%.2f" % (num_agents, eta))
+        plt.xlabel("Trial number")
+        plt.ylabel("Number of steps")
+        plt.savefig(eta_plot_path + "average_curve.png")
+        plt.show()
+    # now analysis for different etas
+    np.savetxt(plot_path + "average_solution_length.txt", average_length_list, fmt="%.6f")
+    np.savetxt(plot_path + "fraction_of_agents.txt", agent_fraction_list, fmt="%.6f")
+    np.savetxt(plot_path + "etas.txt", etas, fmt="%.2f")
     fig, ax1 = plt.subplots()
     ax2 = ax1.twinx()
     # plt.grid()
-    ax1.scatter([0.1, 0.15, 0.2, 0.25, 0.3, 0.35, 0.4, 0.45, 0.5], my_array)
-    ax1.set_ylim(28, 36)
+    ax1.scatter(etas, average_length_list, c="C0")
+    # ax1.set_ylim(28, 36)
     ax1.set_xlabel("glow parameter η")
     ax1.set_ylabel("average number of cumulative_steps", color="C0")
-    ax2.plot([0.1, 0.15, 0.2, 0.25, 0.3, 0.35, 0.4, 0.45, 0.5], amounts, "C1-")
+    ax2.plot(etas, agent_fraction_list, "C1-")
     ax2.set_ylabel("fraction of agents with all 4 solutions", color="C1")
-    ax2.set_ylim(0, 1)
-    # plt.rc('text', usetex=True)
-    # plt.rc('font', family='serif')
-    # plt.xlabel(r"$\eta$")
+    ax2.set_ylim(-0.05, 1.05)
+    plt.savefig(plot_path + "eta_analysis.png")
     plt.show()
 
-exit()
 
-
-# if __name__ == "__main__":
-#     b = np.zeros(64)
-#     for i in range(64):
-#         print(i)
-#         with open("./results/eta_1/step_curve_%d.txt" % i, "r") as f:
-#             step_curve = np.loadtxt(f)
-#         a = step_curve[-2000:]
-#         count = dict(zip(*np.unique(a, return_counts=True)))
-#         b[i] = count_solutions(count)
-#     c = np.zeros(64)
-#     for i in range(64):
-#         print(i)
-#         with open("./results/eta_2/step_curve_%d.txt" % i, "r") as f:
-#             step_curve = np.loadtxt(f)
-#         a = step_curve[-2000:]
-#         count = dict(zip(*np.unique(a, return_counts=True)))
-#         c[i] = count_solutions(count)
-#     d = np.zeros(64)
-#     for i in range(64):
-#         print(i)
-#         try:
-#             with open("./results/eta_3/step_curve_%d.txt" % i, "r") as f:
-#                 step_curve = np.loadtxt(f)
-#         except FileNotFoundError:
-#             print(i, "is missing")
-#             continue
-#         a = step_curve[-2000:]
-#         count = dict(zip(*np.unique(a, return_counts=True)))
-#         d[i] = count_solutions(count)
-#     eff = np.zeros(64)
-#     for i in range(64):
-#         print(i)
-#         try:
-#             with open("./results/eta_4/step_curve_%d.txt" % i, "r") as f:
-#                 step_curve = np.loadtxt(f)
-#         except FileNotFoundError:
-#             print(i, "is missing")
-#             continue
-#         a = step_curve[-2000:]
-#         count = dict(zip(*np.unique(a, return_counts=True)))
-#         eff[i] = count_solutions(count)
-#     plt.hist([b, c, d, eff], bins=[-0.5, 0.5, 1.5, 2.5, 3.5, 4.5])
-#     plt.show()
-#     exit()
-
-
-    # b = np.zeros(64)
-    # mycount = 0
-    # for i in range(64):
-    #     print(i)
-    #     try:
-    #         with open("./results/eta_1/step_curve_%d.txt" % i, "r") as f:
-    #             step_curve = np.loadtxt(f)
-    #     except FileNotFoundError:
-    #         print(i, "is missing")
-    #         continue
-    #
-    #     a = step_curve[-2000:]
-    #     count = dict(zip(*np.unique(a, return_counts=True)))
-    #     auxlist = [(v, k) for k, v in count.items()]
-    #     # print(auxlist)
-    #     auxcount = 0
-    #     auxsteps = 0
-    # b = np.zeros(64)
-    # for i in range(64):
-    #     print(i)
-    #     with open("./results/eta_1/step_curve_%d.txt" % i, "r") as f:
-    #         step_curve = np.loadtxt(f)
-    #     a = step_curve[-2000:]
-    #     count = dict(zip(*np.unique(a, return_counts=True)))
-    #     b[i] = count_solutions(count)
-    # c = np.zeros(64)
-    # for i in range(64):
-    #     print(i)
-    #     with open("./results/eta_2/step_curve_%d.txt" % i, "r") as f:
-    #         step_curve = np.loadtxt(f)
-    #     a = step_curve[-2000:]
-    #     count = dict(zip(*np.unique(a, return_counts=True)))
-    #     c[i] = count_solutions(count)
-    # d = np.zeros(64)
-    # for i in range(64):
-    #     print(i)
-    #     try:
-    #         with open("./results/eta_3/step_curve_%d.txt" % i, "r") as f:
-    #             step_curve = np.loadtxt(f)
-    #     except FileNotFoundError:
-    #         print(i, "is missing")
-    #         continue
-    #     a = step_curve[-2000:]
-    #     count = dict(zip(*np.unique(a, return_counts=True)))
-    #     d[i] = count_solutions(count)
-    # eff = np.zeros(64)
-    # for i in range(64):
-    #     print(i)
-    #     try:
-    #         with open("./results/eta_4/step_curve_%d.txt" % i, "r") as f:
-    #             step_curve = np.loadtxt(f)
-    #     except FileNotFoundError:
-    #         print(i, "is missing")
-    #         continue
-    #     a = step_curve[-2000:]
-    #     count = dict(zip(*np.unique(a, return_counts=True)))
-    #     eff[i] = count_solutions(count)
-    # plt.hist([b, c, d, eff], bins=[-0.5, 0.5, 1.5, 2.5, 3.5, 4.5])
-    # plt.show()
-    # exit()
-    #
-    #
-    # b = np.zeros(64)
-    # mycount = 0
-    # for i in range(64):
-    #     print(i)
-    #     try:
-    #         with open("./results/eta_1/step_curve_%d.txt" % i, "r") as f:
-    #             step_curve = np.loadtxt(f)
-    #     except FileNotFoundError:
-    #         print(i, "is missing")
-    #         continue
-    #
-    #     a = step_curve[-2000:]
-    #     count = dict(zip(*np.unique(a, return_counts=True)))
-    #     auxlist = [(v, k) for k, v in co
-    #     for v, k in auxlist:
-    #         if 400 < v < 600 and int(k) != 0:
-    #             auxcount += 1
-    #             auxsteps += int(k)
-    #         elif 800 < v < 1200 and int(k) != 0:
-    #             auxcount += 2
-    #             auxsteps += 2 * int(k)
-    #     if auxcount == 4:
-    #         b[i] = auxsteps
-    #
-    # c = np.zeros(64)
-    # mycount = 0
-    # for i in range(64):
-    #     print(i)
-    #     try:
-    #         with open("./results/eta_2/step_curve_%d.txt" % i, "r") as f:
-    #             step_curve = np.loadtxt(f)
-    #     except FileNotFoundError:
-    #         print(i, "is missing")
-    #         continue
-    #
-    #     a = step_curve[-2000:]
-    #     count = dict(zip(*np.unique(a, return_counts=True)))
-    #     auxlist = [(v, k) for k, v in count.items()]
-    #     # print(auxlist)
-    #     auxcount = 0
-    #     auxsteps = 0
-    #     for v, k in auxlist:
-    #         if 400 < v < 600 and int(k) != 0:
-    #             auxcount += 1
-    #             auxsteps += int(k)
-    #         elif 800 < v < 1200 and int(k) != 0:
-    #             auxcount += 2
-    #             auxsteps += 2 * int(k)
-    #     if auxcount == 4:
-    #         c[i] = auxsteps
-    #
-    # d = np.zeros(64)
-    # mycount = 0
-    # for i in range(64):
-    #     print(i)
-    #     try:
-    #         with open("./results/eta_3/step_curve_%d.txt" % i, "r") as f:
-    #             step_curve = np.loadtxt(f)
-    #     except FileNotFoundError:
-    #         print(i, "is missing")
-    #         continue
-    #
-    #     a = step_curve[-2000:]
-    #     count = dict(zip(*np.unique(a, return_counts=True)))
-    #     auxlist = [(v, k) for k, v in count.items()]
-    #     # print(auxlist)
-    #     auxcount = 0
-    #     auxsteps = 0
-    #     for v, k in auxlist:
-    #         if 400 < v < 600 and int(k) != 0:
-    #             auxcount += 1
-    #             auxsteps += int(k)
-    #         elif 800 < v < 1200 and int(k) != 0:
-    #             auxcount += 2
-    #             auxsteps += 2 * int(k)
-    #     if auxcount == 4:
-    #         d[i] = auxsteps
-    #
-    # d = np.zeros(64)
-    # mycount = 0
-    # for i in range(64):
-    #     print(i)
-    #     try:
-    #         with open("./results/eta_3/step_curve_%d.txt" % i, "r") as f:
-    #             step_curve = np.loadtxt(f)
-    #     except FileNotFoundError:
-    #         print(i, "is missing")
-    #         continue
-    #
-    #     a = step_curve[-2000:]
-    #     count = dict(zip(*np.unique(a, return_counts=True)))
-    #     auxlist = [(v, k) for k, v in count.items()]
-    #     # print(auxlist)
-    #     auxcount = 0
-    #     auxsteps = 0
-    #     for v, k in auxlist:
-    #         if 400 < v < 600 and int(k) != 0:
-    #             auxcount += 1
-    #             auxsteps += int(k)
-    #         elif 800 < v < 1200 and int(k) != 0:
-    #             auxcount += 2
-    #             auxsteps += 2 * int(k)
-    #     if auxcount == 4:
-    #         d[i] = auxsteps
-    #
-    # plt.hist([b, c, d, eff], bins=np.arange(24.5, 50.5, 1), label=["eta=0.1", "eta=0.2", "eta=0.3", "eta=0.4"])
-    # plt.xticks(np.arange(25, 51))
-    # plt.xlabel("Cumulative number of steps for agents that found 4 solutions")
-    # plt.ylabel("Number of agents")
-    # plt.legend(loc="upper right")
-    # plt.show()
+if __name__ == "__main__":
+    evaluate(result_path_clifford, plot_path_clifford)
+    evaluate(result_path_universal, plot_path_universal)
