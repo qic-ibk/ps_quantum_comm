1a8a37a60ede9bc957072f04a17806475408ebd7
Julius Wallnöfer <33934646+jwallnoefer@users.noreply.github.com>
2018-12-11 16:59:41 +0100

    labels for epp plots

--- utils/epp_evaluation.py
+++ utils/epp_evaluation.py
@@ -18,7 +18,11 @@ if __name__ == "__main__":
     reward_curves = [np.load(result_path + "reward_curve_%d.npy" % i) for i in range(num_agents)]
     average_curve = np.sum(reward_curves, axis=0) / num_agents
     plt.plot(np.arange(1, len(average_curve) * sparsity + 1, sparsity), average_curve)
+    plt.xlabel("Number of trials")
+    plt.ylabel("Average reward.")
     plt.show()
     found_rewards = [reward_curve[-1] for reward_curve in reward_curves]
     plt.hist(found_rewards, bins=30)
+    plt.xlabel("Reward")
+    plt.ylabel("Number of agents")
     plt.show()
