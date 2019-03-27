c7fd77f618bfa2060136e18abb6ed3773e7a1fd3
Julius Wallnöfer <33934646+jwallnoefer@users.noreply.github.com>
2019-03-27 15:49:07 +0100

    try length 8 again with more agents

--- run/run_scaling_delegated.py
+++ run/run_scaling_delegated.py
@@ -5,18 +5,20 @@ import numpy as np
 import itertools as it
 
 num_processes = 64
-num_agents = 128
+num_agents = 512
 num_trials = 10000
 target_fid = 0.9
 result_path = "results/scaling_delegated/raw/"
 
-p_gates = np.arange(0.98, 0.001, 1.001)  # takes about 1 day per data point
+# p_gates = np.arange(0.98, 0.001, 1.001)  # takes about 1 day per data point
+p_gates = [0.999]
 
-setups = [{"repeater_length": 2, "allowed_block_lengths": [], "start_fids": list(it.product(np.arange(0.6, 1.00, 0.05), repeat=2))},
-          {"repeater_length": 3, "allowed_block_lengths": [2], "start_fids": list(it.product(np.arange(0.6, 1.00, 0.1), repeat=3))},
-          {"repeater_length": 4, "allowed_block_lengths": [2, 3], "start_fids": list(it.product(np.arange(0.6, 1.00, 0.1), repeat=4))},
-          {"repeater_length": 8, "allowed_block_lengths": [2, 3, 4], "start_fids": [(0.7,) * 8, (0.8, 0.6, 0.8, 0.8, 0.7, 0.8, 0.8, 0.6), (0.95, 0.9, 0.6, 0.9, 0.95, 0.95, 0.9, 0.6)]}
-          ]
+setups = [{"repeater_length": 8, "allowed_block_lengths": [2, 3, 4], "start_fids": [(0.7,) * 8, (0.8, 0.6, 0.8, 0.8, 0.7, 0.8, 0.8, 0.6), (0.95, 0.9, 0.6, 0.9, 0.95, 0.95, 0.9, 0.6)]}]
+         #[{"repeater_length": 2, "allowed_block_lengths": [], "start_fids": list(it.product(np.arange(0.6, 1.00, 0.05), repeat=2))},
+         # {"repeater_length": 3, "allowed_block_lengths": [2], "start_fids": list(it.product(np.arange(0.6, 1.00, 0.1), repeat=3))},
+         # {"repeater_length": 4, "allowed_block_lengths": [2, 3], "start_fids": list(it.product(np.arange(0.6, 1.00, 0.1), repeat=4))},
+         # {"repeater_length": 8, "allowed_block_lengths": [2, 3, 4], "start_fids": [(0.7,) * 8, (0.8, 0.6, 0.8, 0.8, 0.7, 0.8, 0.8, 0.6), (0.95, 0.9, 0.6, 0.9, 0.95, 0.95, 0.9, 0.6)]}
+         # ]
 
 if __name__ == "__main__":
     for p in p_gates:
