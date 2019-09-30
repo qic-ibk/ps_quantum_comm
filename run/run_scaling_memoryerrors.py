aecac3faaf837b6e48ca2c58e48296a3c225e67c
Julius Wallnöfer <33934646+jwallnoefer@users.noreply.github.com>
2019-09-30 18:19:13 +0200

    adds runs for different station positions

--- run/run_scaling_memoryerrors.py
+++ run/run_scaling_memoryerrors.py
@@ -1,8 +1,9 @@
 from __future__ import division, print_function
 import os, sys; sys.path.insert(0, os.path.abspath("."))
-from run.aux_scaling_memoryerrors import run_scaling_fids, run_scaling_distances
+from run.aux_scaling_memoryerrors import run_scaling_fids, run_scaling_distances, SolutionCollection
 import numpy as np
 import itertools as it
+from warnings import warn
 
 num_processes = 64
 num_agents = 128
@@ -37,3 +38,52 @@ if __name__ == "__main__":
         # run_scaling_distances(num_processes=num_processes, num_agents=num_agents, num_trials=num_trials,
         #                       distances=test_setup["distances"], allowed_block_lengths=test_setup["allowed_block_lengths"],
         #                       p_gates=p, memory_alpha=memory_alpha, target_fid=target_fid, result_path=p_path)
+
+
+# # Different length distribution run
+
+total_length = 30000  # 30km
+num_possible_stations = 7
+possible_stations = [x * total_length / (num_possible_stations + 1) for x in range(num_possible_stations + 2)]
+alpha = 1 / 0.1
+p = 0.99
+target_fid = 0.9
+position_path = "results/scaling_memoryerrors_positions/raw/" + "p_gates" + str(int(p * 1000)) + "_alpha" + str(int(alpha)) + "/"
+sc_path = "results/scaling_memoryerrors/raw/" + "p_gates" + str(int(p * 1000)) + "_alpha" + str(int(alpha)) + "/"
+
+# # first, try to obtain solution collection from runs above
+
+
+def distances_from_station_numbers(numbers):
+    aux1 = [0] + list(numbers)
+    aux2 = list(numbers) + [num_possible_stations + 1]
+    distances = [j - i for i, j in zip(aux1, aux2)]
+    return distances
+
+
+# take the solution collection from the above results
+sc = SolutionCollection()
+try:
+    sc.load(sc_path + "/solution_collection.pickle")
+except IOError:
+    warn("SolutionCollection not found - creating new one.")
+sc.save(result_path + "solution_collection.pickle")
+
+
+# one additional station
+distances = [distances_from_station_numbers([i]) for i in range(1, num_possible_stations + 1)]
+run_scaling_distances(num_processes=num_processes, num_agents=num_agents, num_trials=num_trials,
+                      distances=distances_from_station_numbers, allowed_block_lengths=[],
+                      p_gates=p, memory_alpha=alpha, target_fid=target_fid, result_path=position_path)
+
+# two additional stations
+distances = [distances_from_station_numbers([i, j]) for i in range(1, num_possible_stations) for j in range(i + 1, num_possible_stations + 1)]
+run_scaling_distances(num_processes=num_processes, num_agents=num_agents, num_trials=num_trials,
+                      distances=distances_from_station_numbers, allowed_block_lengths=[2],
+                      p_gates=p, memory_alpha=alpha, target_fid=target_fid, result_path=position_path)
+
+# three additional stations
+distances = [distances_from_station_numbers([i, j, k]) for i in range(1, num_possible_stations - 1) for j in range(i + 1, num_possible_stations) for k in range(j + 1, num_possible_stations + 1)]
+run_scaling_distances(num_processes=num_processes, num_agents=num_agents, num_trials=num_trials,
+                      distances=distances_from_station_numbers, allowed_block_lengths=[2, 3],
+                      p_gates=p, memory_alpha=alpha, target_fid=target_fid, result_path=position_path)
