3e905e76526f05822a48bba239ae049ffc1aeeb1
Julius Wallnöfer <33934646+jwallnoefer@users.noreply.github.com>
2019-10-09 17:18:05 +0200

    setup finished to run positions

--- run/run_scaling_memoryerrors.py
+++ run/run_scaling_memoryerrors.py
@@ -11,33 +11,33 @@ num_trials = 10000
 target_fid = 0.9
 result_path = "results/scaling_memoryerrors/raw/"
 
-# p_gates = np.arange(0.98, 0.001, 1.001)  # takes about 1 day per data point
-p_gates = [0.99]
-# memory_alphas = [1.0 / 1, 1.0 / 0.5]  # one second/half a second
-memory_alphas = [1.0 / 0.1, 1.0 / 0.01, 1.0 / 0.001]
-
-
-setups = [{"allowed_block_lengths": [], "start_fids": list(it.product(np.arange(0.6, 1.00, 0.05), repeat=2))},
-          {"allowed_block_lengths": [2], "start_fids": list(it.product(np.arange(0.6, 1.00, 0.1), repeat=3))},
-          {"allowed_block_lengths": [2, 3], "start_fids": list(it.product(np.arange(0.6, 1.00, 0.1), repeat=4))},
-          # {"allowed_block_lengths": [2, 3, 4], "start_fids": [(0.7,) * 8, (0.8, 0.6, 0.8, 0.8, 0.7, 0.8, 0.8, 0.6), (0.95, 0.9, 0.6, 0.9, 0.95, 0.95, 0.9, 0.6)]}
-          {"allowed_block_lengths": [2, 3, 4], "start_fids": [(0.8, 0.6, 0.8, 0.8, 0.7, 0.8, 0.8, 0.6)]}
-          ]
-
-# setups = [{"start_fids": list(it.product(np.arange(0.6, 1.00, 0.05), repeat=2)), "allowed_block_lengths": []}]
-# test_setup = {"distances": [(0.8, 0.6, 0.8, 0.8, 0.7, 0.8, 0.8, 0.6)], "allowed_block_lengths": [2, 3, 4]}
-
-if __name__ == "__main__":
-    for p in p_gates:
-        for alpha in memory_alphas:
-            p_path = result_path + "p_gates" + str(int(p * 1000)) + "_alpha" + str(int(alpha)) + "/"
-            for setup in setups:
-                run_scaling_fids(num_processes=num_processes, num_agents=num_agents, num_trials=num_trials,
-                                 start_fids=setup["start_fids"], allowed_block_lengths=setup["allowed_block_lengths"],
-                                 p_gates=p, memory_alpha=alpha, target_fid=target_fid, result_path=p_path)
-        # run_scaling_distances(num_processes=num_processes, num_agents=num_agents, num_trials=num_trials,
-        #                       distances=test_setup["distances"], allowed_block_lengths=test_setup["allowed_block_lengths"],
-        #                       p_gates=p, memory_alpha=memory_alpha, target_fid=target_fid, result_path=p_path)
+# # p_gates = np.arange(0.98, 0.001, 1.001)  # takes about 1 day per data point
+# p_gates = [0.99]
+# # memory_alphas = [1.0 / 1, 1.0 / 0.5]  # one second/half a second
+# memory_alphas = [1.0 / 0.1, 1.0 / 0.01, 1.0 / 0.001]
+#
+#
+# setups = [{"allowed_block_lengths": [], "start_fids": list(it.product(np.arange(0.6, 1.00, 0.05), repeat=2))},
+#           {"allowed_block_lengths": [2], "start_fids": list(it.product(np.arange(0.6, 1.00, 0.1), repeat=3))},
+#           {"allowed_block_lengths": [2, 3], "start_fids": list(it.product(np.arange(0.6, 1.00, 0.1), repeat=4))},
+#           # {"allowed_block_lengths": [2, 3, 4], "start_fids": [(0.7,) * 8, (0.8, 0.6, 0.8, 0.8, 0.7, 0.8, 0.8, 0.6), (0.95, 0.9, 0.6, 0.9, 0.95, 0.95, 0.9, 0.6)]}
+#           {"allowed_block_lengths": [2, 3, 4], "start_fids": [(0.8, 0.6, 0.8, 0.8, 0.7, 0.8, 0.8, 0.6)]}
+#           ]
+#
+# # setups = [{"start_fids": list(it.product(np.arange(0.6, 1.00, 0.05), repeat=2)), "allowed_block_lengths": []}]
+# # test_setup = {"distances": [(0.8, 0.6, 0.8, 0.8, 0.7, 0.8, 0.8, 0.6)], "allowed_block_lengths": [2, 3, 4]}
+#
+# if __name__ == "__main__":
+#     for p in p_gates:
+#         for alpha in memory_alphas:
+#             p_path = result_path + "p_gates" + str(int(p * 1000)) + "_alpha" + str(int(alpha)) + "/"
+#             for setup in setups:
+#                 run_scaling_fids(num_processes=num_processes, num_agents=num_agents, num_trials=num_trials,
+#                                  start_fids=setup["start_fids"], allowed_block_lengths=setup["allowed_block_lengths"],
+#                                  p_gates=p, memory_alpha=alpha, target_fid=target_fid, result_path=p_path)
+#         # run_scaling_distances(num_processes=num_processes, num_agents=num_agents, num_trials=num_trials,
+#         #                       distances=test_setup["distances"], allowed_block_lengths=test_setup["allowed_block_lengths"],
+#         #                       p_gates=p, memory_alpha=memory_alpha, target_fid=target_fid, result_path=p_path)
 
 
 # # Different length distribution run
@@ -66,7 +66,9 @@ sc = SolutionCollection()
 try:
     sc.load(sc_path + "/solution_collection.pickle")
 except IOError:
-    warn("SolutionCollection not found - creating new one.")
+    # warn("SolutionCollection not found - creating new one.")
+    warn("Expected existing SolutionCollection not found.")
+    raise
 sc.save(result_path + "solution_collection.pickle")
 
 
