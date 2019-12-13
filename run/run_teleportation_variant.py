31f568001d6addcd4d2bfe0a4b2db10e7f012e2a
Julius Wallnöfer <33934646+jwallnoefer@users.noreply.github.com>
2019-12-13 17:29:15 +0100

    rerun with more trials

--- run/run_teleportation_variant.py
+++ run/run_teleportation_variant.py
@@ -26,7 +26,7 @@ num_processes = 64  # change according to cluster computer you choose
 num_agents = 500
 # etas = [0.1, 0.15, 0.2, 0.25, 0.3, 0.35, 0.4, 0.45, 0.5, 0.55, 0.6, 0.65, 0.7]
 etas = [0.3]
-n_trials = 10**6
+n_trials = 5 * 10**6
 result_path = "results/teleportation/variant2/raw/"
 
 
