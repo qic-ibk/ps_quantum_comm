0c5ca8a35e5126abf5525a6aa8548c2a005b8b16
Julius Wallnöfer <33934646+jwallnoefer@users.noreply.github.com>
2020-01-27 17:44:00 +0100

    improved evaluation

--- utils/scaling_memoryerrors_evaluation.py
+++ utils/scaling_memoryerrors_evaluation.py
@@ -22,21 +22,28 @@ assert_dir(output_path)
 #     f.write("learning curves are plotted for p=0.99")
 # np.savetxt(output_path + "ps.txt", ps)
 
-alphas = [1, 2, 10]  # , 100, 1000]
-colors = ["C0", "C1", "C2", "C3"]  # , "C4", "C5"]
+# alphas = [1, 2, 10]  # , 100, 1000]
+# colors = ["C0", "C1", "C2", "C3"]  # , "C4", "C5"]
+alphas = [1, 2, 11, 12, 14, 16, 20, 25, 26, 27, 28, 29, 30]  # , 33, 50]
+colors = ["C0", "C1", "C2", "C3", "C4", "C5", "C6", "C7", "C8", "C9", "C0", "C1", "C2", "C3"]
+# alphas = [25, 26, 27, 28, 29, 30] # , 31, 32]
 
 resource_list = []
+const_list = []
 for i, alpha in enumerate(alphas):
     resources = np.load(results_path + "p_gates990_alpha%d/length8_0/best_resources.npy" % alpha)
     resource_list += [resources[-1]]
     plt.scatter(np.arange(1, len(resources) + 1), resources, s=20, color=colors[i], label="alpha=%d" % alpha)
     const = naive_constant(start_fid=(0.8, 0.6, 0.8, 0.8, 0.7, 0.8, 0.8, 0.6), target_fid=0.9, p_gates=0.99, memory_alpha=alpha)
+    const_list += [const]
     plt.axhline(y=const, color=colors[i])
 # also plot no memory errors
 resources = np.load("results/scaling_delegated/raw/" + "p_gates990/length8_1/best_resources.npy")
 plt.scatter(np.arange(1, len(resources) + 1), resources, s=20, color=colors[-1], label="alpha=0")
 const_nomemory = naive_constant_nomemory(repeater_length=8, start_fid=(0.8, 0.6, 0.8, 0.8, 0.7, 0.8, 0.8, 0.6), target_fid=0.9, p_gates=0.99)
 plt.axhline(y=const_nomemory, color=colors[-1])
+
+print(naive_constant(start_fid=(0.8, 0.6, 0.8, 0.8, 0.7, 0.8, 0.8, 0.6), target_fid=0.9, p_gates=0.99, memory_alpha=30))
 
 plt.title("different memory times for: " + str((0.8, 0.6, 0.8, 0.8, 0.7, 0.8, 0.8, 0.6)))
 plt.ylabel("resources used")
@@ -47,6 +54,14 @@ plt.legend()
 plt.savefig(output_path + "differing_alphas.png")
 plt.show()
 
+plt.title("required resources for different alphas")
+plt.ylabel("resources used")
+plt.xlabel("memory quality alpha")
+plt.yscale("log")
+plt.scatter([0] + alphas, [resources[-1]] + resource_list)
+plt.scatter([0] + alphas, [const_nomemory] + const_list)
+plt.grid()
+plt.show()
 # print(naive_constant(start_fid=(0.8, 0.6, 0.8, 0.8, 0.7, 0.8, 0.8, 0.6), target_fid=0.9, p_gates=0.99, memory_alpha=100))  # => nan, so the naive strategy does not provide a solution either
 
 
